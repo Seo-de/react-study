94680e766d43c35f40093b459b2d212472fd3cca
Seo-de <110797113+Seo-de@users.noreply.github.com>
2023-06-08 11:06:16 +0900

    커밋한다

--- nomard_movie/src/App.js
+++ nomard_movie/src/App.js
@@ -4,7 +4,7 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       {/*  Route를 찾아 컴포넌트 렌더링, 한 번에 하나의 Route만 렌더링 위함함 */}
       <Routes>
         {/* Route 안에 컴포넌트를 적어줌 // Home으로 가도록 설정 */}
